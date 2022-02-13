8d06e8341ecd72dc0a422826a7873059df178286
volyayana <volya_y@mail.ru>
2022-02-13 21:17:40 +0300

    Реализованы 3 view функции, настроены правильные урлы.
    / - домашняя страница, содержит список доступных страниц;
    current_time/ - показывает текущее время в любом удобном вам формате;
    workdir/ – выводит содержимое рабочей директории.

---
+++ first-project/first_project/app/views.py
@@ -0,0 +1,32 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    dir_list = '</ul><ul>'.join(os.listdir())
+    return HttpResponse(f'<b>Список файлов в рабочей директории:</b> <ul>{dir_list}</ul>')
